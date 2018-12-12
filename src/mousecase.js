1a98ff9cd2cd483305a429612c3b2cabb1f89f17
Jeff Wainwright <yowainwright@gmail.com>
2018-12-11 19:57:09 -0800

    [update] adds `hopefully` working mousecase

--- src/mousecase.js
+++ src/mousecase.js
@@ -1,25 +1,29 @@
-
+/**
+ * debug
+ * @param {msg} string
+ * provides useful mousecase messaging
+ */
 export const debug = (msg) => console.warn('%c MouseCase 🐹:', 'background-color: #FFB6C1; color: white', ` ${msg}`)
 
-const objectToString = (o) => JSON.stringify(o)
+/**
+ * objectToString
+ * @param {o} object
+ * turns object into string for easier debugging
+ */
+export const objectToString = (o) => JSON.stringify(o)
 
-export const events = [
-  'mousemove',
-  'mousedown',
-  'mouseleave',
-  'mouseup',
-  'mousemove',
-]
 class MouseCase {
   constructor (
     target,
-    props,
-  ) {
-    this.state = {
-      mouseIsDown: false,
-      startx: null,
-      scrollLeft: null,
+    props = {
+      debug: false,
+      cssClass: 'js-mousecase',
+      rule: true,
     }
+  ) {
+    /**
+     * warnings
+     */
     const el = typeof target === 'string' ? document.querySelector(target) : target
     if (!el) {
       if (props.debug) debug('no target element is defined')
@@ -31,36 +35,72 @@ class MouseCase {
       if (props.debug) debug(`${props.rule} boolean is false; MouseCase is not running`)
       return
     }
+
+    /**
+     * initial state
+     */
+    this.state = {
+      isDown: false,
+      startx: null,
+      scrollLeft: null,
+    }
+
+    /**
+     * initial props
+     */
     this.props = {
+      activeClass: `${props.cssClass}--is-active`,
       el,
-      debug: props.debug || false,
-      cssClass: props.cssClass || 'js-mousecase',
-      rule: props.rule || null,
+      debug: props.debug,
+      cssClass: props.cssClass,
+      rule: props.rule,
     }
     this.props.el.classList.add(this.props.cssClass)
-    this.manageMouseCaseState()
+    if (this.props.rule) this.this.manageState()
     return this
   }
 
   /**
-   * manageState 👩🏽‍🎨
-   * @returns this
-   */
-  manageMouseCaseState () {
-    const { cssClass, el } = this.props
-    const mouseCaseIsActiveCssClass = `${cssClass}--is-active`
-    events.map(e => {
-      el.addEventListener(e, () => {
-        if (e === 'mousedown') {
-          el.classList.add(mouseCaseIsActiveCssClass)
-          this.state.startx = this.state.startx - el.offsetLeft
-          this.state.scrollLeft = el.scrollLeft
-        } else {
-          el.classList.remove(mouseCaseIsActiveCssClass)
-        }
-        if (this.props.debug) debug(`${e} is invoked; state: ${objectToString(this.state)}, props: ${objectToString(this.props)}`)
-      })
-    })
+    * MouseDown
+    * @param {e} event
+    * what happens when the mouse is down
+    */
+  mouseDown (e) {
+    const { el } = this.props
+    el.classList.add('active')
+    this.state.isDown = true
+    this.state.startX = e.pageX - el.offsetLeft
+    this.state.scrollLeft = el.scrollLeft
+    return this
+  }
+
+  /**
+    * MouseMove
+    * @param {e} event
+    * what happens when the mouse moves
+    */
+  mouseMove (e) {
+    if (!this.state.isDown) {
+      this.state.isDown = false
+      return
+    }
+    const { el } = this.props
+    e.preventDefault()
+    const initial = e.pageX - el.offsetLeft
+    const distance = (initial - this.state.startX) * 3
+    el.scrollLeft = this.state.scrollLeft - distance
+    return this
+  }
+
+  /**
+    * ManageState
+    * manages mouseCase state
+    */
+  manageState () {
+    const { el } = this.props
+    el.addEventListener('mousemove', (e) => this.mouseMove(e))
+    el.addEventListener('mousedown', (e) => this.mouseDown(e))
+    if (this.props.debug) el.addEventListener('mouseup', debug(`state: ${objectToString(this.state)}, props: ${objectToString(this.props)}`))
     return this
   }
 }
