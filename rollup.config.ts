983a5b14a6dbf5ddb00fc082adfc0d006c7f8455
yowainwright <yowainwright@gmail.com>
2022-01-24 00:43:17 -0800

    feat: beta major version bump

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,4 +1,11 @@
-import { author, description, homepage, license, name, version } from './package.json'
+import {
+  author,
+  description,
+  homepage,
+  license,
+  name,
+  version,
+} from './package.json'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
@@ -15,24 +22,28 @@ const banner = `/**
 const plugins = [
   resolve(),
   commonjs(),
-  typescript({ tsconfig: false, lib: ['esnext', 'dom', 'dom.iterable'], target: 'es5' }),
+  typescript({
+    tsconfig: false,
+    lib: ['esnext', 'dom', 'dom.iterable'],
+    target: 'es5',
+  }),
 ]
 
 const inputs = ['reframe', 'noframe', 'jquery.reframe', 'jquery.noframe']
 const esRollups = inputs.map((name) => ({
-  input: `src/${name}.ts`,
+  input: `src/index.ts`,
   output: { banner, name, file: `dist/${name}.es.js`, format: 'es' },
   plugins,
 }))
 
 const umdRollups = inputs.map((name) => ({
-  input: `src/${name}.ts`,
+  input: `src/index.ts`,
   output: { banner, name, file: `dist/${name}.js`, format: 'umd' },
   plugins,
 }))
 
 const minRollups = inputs.map((name) => ({
-  input: `src/${name}.ts`,
+  input: `src/index.ts`,
   output: { banner, name, file: `dist/${name}.min.js`, format: 'umd' },
   plugins: [...plugins, uglify()],
 }))
